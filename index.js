81fe307a9539e6e7fafe5ea018ad0bec31eb4657
Ahmed Hussin <ahmed208087@gmail.com>
2022-03-05 23:59:11 +0200

    Personal-Website-1.2

--- index.js
+++ index.js
@@ -1,5 +1,21 @@
-const linkedin = document.getElementById('#linked')
-// when clicking on the div it take you to the linked in profile 
+const linkedin = document.getElementById('linked')
+// when clicking on the div it take you to the linked in profile
 linkedin.addEventListener('click',()=> {
     window.location.href='https://www.linkedin.com/in/ahmed-mohamed-hussein-b6b552202/'
 })
+
+// closing and opening the model the model
+let open = document.querySelector('#open')
+let model= document.querySelector('#model-container');
+let closeBtn= document.querySelector('#close-btn');
+
+
+open.addEventListener('click', ()=>{
+    model.style.opacity="1"
+    model.style.pointerEvents="auto"
+});
+
+closeBtn.addEventListener('click', ()=>{
+    model.style.opacity="0"
+    model.style.pointerEvents="none"
+});
